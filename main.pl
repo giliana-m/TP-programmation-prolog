c8c9cecfb770d62b328efcabde6f15f9ede75964
giliana-m <markandugiliana@gmail.com>
2025-01-08 12:48:33 +0100

    Update main.pl

--- main.pl
+++ main.pl
@@ -20,6 +20,7 @@ mere(X, Y) :- femme(X), parent(X, Y).
 main :-
     (   pere(pierre, paul), write('Pierre est le père de Paul.'), nl, % Qui est le père de Paul ?
         mere(marie, paul), write('Marie est la mère de Paul.'), nl,  % Qui est la mère de Paul ?
-        parent(marc, sophie), write('Marc est le parent de Sophie.'), nl  % Marc est-il le parent de Sophie ?
+        parent(marc, sophie), write('Marc est le parent de Sophie.'), nl,  % Marc est-il le parent de Sophie ?
+        pere(jacques, marc), write('Jacques est le père de Marc'), nl % Qui est le père de Marc ?
     ).
     
