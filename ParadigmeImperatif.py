7f181f5fb1ecb4347fa547ce58cebd97c008e109
giliana-m <markandugiliana@gmail.com>
2025-01-17 14:49:13 +0100

    Partie 2

--- ParadigmeImperatif.py
+++ ParadigmeImperatif.py
@@ -1,10 +1,10 @@
 # Partie 1 : Saisie des données
 def saisir_donnees():
-    n = int(input("Combien d'étudiants souhaitez-vous saisir ? "))  # pose la question sur le nombre étudiants
+    n = int(input("Combien d'étudiants souhaitez-vous saisir ? "))  # nombre d'étudiants
     noms = []  # Liste noms des étudiants
     notes = []  # Liste notes des étudiants
     
-    for i in range(n): #boucle qui se repete n fois
+    for i in range(n):
         nom = input(f"Nom de l’étudiant {i + 1} : ")  # nom de l'étudiant
         note = float(input(f"Note de {nom} : "))  # note de l'étudiant
         noms.append(nom)  # Ajoute nom à la liste
@@ -12,6 +12,20 @@ def saisir_donnees():
     
     return noms, notes  # Retourne deux listes
 
-noms, notes = saisir_donnees()
-print("Noms des étudiants :", noms)
-print("Notes des étudiants :", notes)
+
+# Partie 2 : Calcul de la moyenne
+def calcul_moyenne(notes):
+    return sum(notes) / len(notes) if len(notes) > 0 else 0  # Moyenne des notes
+
+def main():
+#partie 1
+    noms, notes = saisir_donnees()
+    print("Noms des étudiants :", noms)
+    print("Notes des étudiants :", notes)
+
+#partie 2
+    moyenne = calcul_moyenne(notes)  # Calcul de la moyenne
+    print(f"La moyenne de la classe est de {moyenne:.2f}.")  # Affiche Moyenne, f-string, variables dans une chaîne de caractères arrondit à 2 décimales
+
+if __name__ == "__main__":
+    main()
