6be24249eab189056cdade984190cc61e1e2aba2
giliana-m <markandugiliana@gmail.com>
2025-02-04 12:35:58 +0100

    cloture.py

--- cloture.py
+++ cloture.py
@@ -25,3 +25,16 @@ print(squares)  #résultat sera : [1, 4, 9, 16, 25]
 somme = lambda a, b: a + b
 print(somme(2, 6))  #résultat : 8
 
+
+from functools import reduce
+
+#Expression lambda pour additionner deux nombres
+somme = lambda a, b: a + b
+
+#Liste
+numbers = [1, 2, 3, 4, 5]
+
+#reduce pour réduire la liste en somme totale
+total = reduce(somme, numbers)
+
+print(total)  #résultat = 15
